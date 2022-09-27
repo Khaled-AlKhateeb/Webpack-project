c1365b6cc05cf92aa17a9e018f9a66281822c119
Khaled-AlKhateeb <khaled.w.khateeb85@gmail.com>
2022-09-27 15:08:51 +0300

    one commit to rule them all

---
+++ dist/print.bundle.js
@@ -0,0 +1,19 @@
+(self["webpackChunkwebpack_project"] = self["webpackChunkwebpack_project"] || []).push([["print"],{
+
+/***/ "./src/print.js":
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log('I get called from print.js!');
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLXByb2plY3QvLi9zcmMvcHJpbnQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0kgZ2V0IGNhbGxlZCBmcm9tIHByaW50LmpzIScpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
